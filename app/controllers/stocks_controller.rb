9015977f2889420943b7a39dccadb023298eaff8
Dmitry Khromov <devandart@ya.ru>
2015-11-27 16:51:32 +0300

    prepere response

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,10 +3,15 @@ class StocksController < ApplicationController
 
   def index
     @stocks = current_user.stocks
+    yahoo_client = YahooFinance::Client.new
+    @data = {}
+    @stocks.map(&:name).each do |a|
+      response = yahoo_client.historical_quotes(a.name, { start_date: 2.years.ago, end_date: Time::now })
+      @data[a] = JSON.parse(response.to_json).collect {|a| a['table'].merge(a[close] * a.num)}
+    end
   end
 
   def show
     @stock = current_user.stocks.find(params[:id])
-
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.destroy_all
 user = User.create(email: 'example@gmail.com', password: 'root0000', password_confirmation: 'root0000')
-["yhoo", "goog"].each do |sym|
+["YHOO", "GOOG"].each do |sym|
   user.stocks.create({name: sym, num: rand(99) + 1,})
 end
